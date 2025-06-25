0fc61c3bf1a670eaa92ce73d240931755c3b73cc
Renan Vila Bela <vilabelarenan@gmail.com>
2025-06-25 15:38:16 -0300

    explorando um pouco mais estruturas de controle

--- Fundamentos/estrutura-controle.py
+++ Fundamentos/estrutura-controle.py
@@ -28,3 +28,42 @@ contador = 1
 while contador <= 5:
     print(f"Contador: {contador}")
     contador += 1  # Incrementa o contador
+
+#for - laço de repetição
+#Itera sobre uma sequência (lista, tupla, string, etc.)
+nomes = ["Ana", "Mariana", "Renan"]
+
+for nome in nomes:
+    print(f"Olá, {nome}!")
+#com range - gera uma sequência de números
+for i in range(1, 6):  # De 1 a 5
+    print(f"Número: {i}")
+#range(início, fim) --> fim não incluído
+
+#palavras-chave complementares:
+#break: interrompe o laço antes do fim
+#continue: pula para a próxima iteração do laço
+#pass: não faz nada, serve como um espaço reservado (placeholder)
+#Exemplo de break e continue
+for numero in range(1, 10):
+    if numero == 5:
+        break  # Interrompe o laço
+    print(f"Número: ", numero)
+
+for numero in range(1, 6):
+    if numero == 3:
+        continue  # Pula o número 3
+    print(f"Número: ", numero)
+
+#Estrutura de controle match
+#funciona como um switch/case em outras linguagens
+comando = "iniciar"
+
+match comando:
+    case "iniciar":
+        print("Iniciando o sistema...")
+    case "parar":
+        print("Parando o sistema...")
+    case _:
+        print("Comando desconhecido.")
+#case_: funciona como o "default" em outras linguagens, capturando qualquer outro caso não especificado.
