861bd2459f38be71b4d89475e5f7300071d4b12a
Renan Vila Bela Assis de Almeida <renan.almeida@ati.intranet>
2025-06-26 10:18:25 -0300

    criando exercicios c funções

---
+++ Fundamentos/exercicio-funcoes.py
@@ -0,0 +1,16 @@
+def boas_vindas(nome):
+    print("Olá, " + nome + "! Seja bem-vindo(a) ao nosso programa.")
+boas_vindas("Renan")
+
+def soma_dois_numeros(a, b):
+    return a + b
+resultado = soma_dois_numeros(5, 7)
+print("A soma é:", resultado)
+
+def fatorial(n):
+    resultado = 1
+    for i in range(2, n + 1):
+        resultado *= i
+    return resultado
+fatorial
+
