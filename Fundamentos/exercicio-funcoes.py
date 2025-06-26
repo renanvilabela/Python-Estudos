af1c32b85b9577db059a50bb6cb2945ee81d542b
Renan Vila Bela Assis de Almeida <105953125+renanvilabela@users.noreply.github.com>
2025-06-26 13:40:34 -0300

    conclusão exercícios python

--- Fundamentos/exercicio-funcoes.py
+++ Fundamentos/exercicio-funcoes.py
@@ -8,10 +8,30 @@ resultado = soma_dois_numeros(5, 7)
 print("A soma é:", resultado)
 
 def fatorial(n):
+    if n < 0:
+        return "Fatorial não definido para números negativos."
     resultado = 1
     for i in range(2, n + 1):
         resultado *= i
     return resultado
-fatorial
+print(fatorial(5))
 
+def calcular_desconto(preco, porcentagem=10):
+    desconto = preco * (porcentagem / 100)
+    preco_final = preco - desconto
+    return preco_final
+print(calcular_desconto(100))
+print(calcular_desconto(200, 20))
 
+def soma_total(*valores):
+    total = 0
+    for valor in valores:
+        total += valor
+    return total
+print(soma_total(1, 2, 3, 4, 5))
+
+def exibir_usuario(**dados):
+    print("Dados do usuário:")
+    for chave, valor in dados.items():
+        print(f"{chave.capitalize()}: {valor}")
+exibir_usuario(nome="Renan", idade=21, cidade="recife")
