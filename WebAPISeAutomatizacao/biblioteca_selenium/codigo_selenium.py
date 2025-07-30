8835669df94e6f908564a737dc2c7135f771d0fb
Renan Vila Bela <vilabelarenan@gmail.com>
2025-07-30 14:22:51 -0300

    correções, forçando o click via js

--- WebAPISeAutomatizacao/biblioteca_selenium/codigo_selenium.py
+++ WebAPISeAutomatizacao/biblioteca_selenium/codigo_selenium.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-#abrir o navegador
+# abrir o navegador
 navegador = webdriver.Chrome()
-
-# acessar um site
 navegador.get("https://www.hashtagtreinamentos.com/")
-
 navegador.maximize_window()
-botao_verde = navegador.find_element("class name", "botao-verde")
 
-botao_verde.click()
+# espera o botão carregar
+wait = WebDriverWait(navegador, 20)
+botao_verde = wait.until(EC.presence_of_element_located((By.XPATH, '//a[contains(@class, "botao-verde")]')))
+
+# força o clique via JavaScript
+navegador.execute_script("arguments[0].click();", botao_verde)
 
 time.sleep(10)
